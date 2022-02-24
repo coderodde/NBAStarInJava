881763a7239745e430c94db64c0ff3168266f65d
coderodde <coderodd3@gmail.com>
2022-02-24 14:42:12 +0200

    Initial commit.

---
+++ src/main/java/Demo.java
@@ -0,0 +1,148 @@
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import net.coderodde.graph.DirectedGraph;
+import net.coderodde.graph.DirectedGraphWeightFunction;
+import net.coderodde.graph.pathfinding.AbstractPathfinder;
+import net.coderodde.graph.pathfinding.DirectedGraphNodeCoordinates;
+import net.coderodde.graph.pathfinding.HeuristicFunction;
+import net.coderodde.graph.pathfinding.support.AStarPathfinder;
+import net.coderodde.graph.pathfinding.support.DijkstraPathfinder;
+import net.coderodde.graph.pathfinding.support.EuclideanHeuristicFunction;
+import net.coderodde.graph.pathfinding.support.NBAStarPathfinder;
+
+public class Demo {
+
+    private static final int NODES = 100_000;
+    private static final int ARCS = 500_000;
+
+    public static void main(String[] args) {
+        long seed = System.nanoTime();
+        Random random = new Random(seed);
+        System.out.println("Seed = " + seed);
+
+        long start = System.currentTimeMillis();
+        DirectedGraph graph = getRandomGraph(NODES, ARCS, random);
+        DirectedGraphNodeCoordinates coordinates = getCoordinates(graph, 
+                                                                  random);
+        DirectedGraphWeightFunction weightFunction = 
+                getWeightFunction(graph, coordinates);
+
+        List<Integer> graphNodeList = new ArrayList<>(graph.getNodeList());
+
+        Integer sourceNodeId = choose(graphNodeList, random);
+        Integer targetNodeId = choose(graphNodeList, random);
+        long end = System.currentTimeMillis();
+
+        System.out.println("Created the graph data structures in " +
+                           (end - start) + " milliseconds.");
+
+        System.out.println("Source: " + sourceNodeId);
+        System.out.println("Target: " + targetNodeId);
+
+        System.out.println();
+
+        HeuristicFunction hf = new EuclideanHeuristicFunction(coordinates);
+
+        AbstractPathfinder finder1 = new AStarPathfinder(graph,
+                                                         weightFunction,
+                                                         hf);
+
+        AbstractPathfinder finder2 = new DijkstraPathfinder(graph,
+                                                            weightFunction);
+
+        AbstractPathfinder finder3 = new NBAStarPathfinder(graph, 
+                                                           weightFunction,
+                                                           hf);
+        start = System.currentTimeMillis();
+        List<Integer> path1 = finder1.search(sourceNodeId, targetNodeId);
+        end = System.currentTimeMillis();
+
+        System.out.println("A* in " + (end - start) + " milliseconds.");
+
+        path1.forEach(System.out::println);
+        System.out.println();
+
+        start = System.currentTimeMillis();
+        List<Integer> path2 = finder2.search(sourceNodeId, targetNodeId);
+        end = System.currentTimeMillis();
+
+        System.out.println("Dijkstra in " + (end - start) + " milliseconds.");
+        path2.forEach(System.out::println);
+        System.out.println();
+
+        start = System.currentTimeMillis();
+        List<Integer> path3 = finder3.search(sourceNodeId, targetNodeId);
+        end = System.currentTimeMillis();
+
+        System.out.println("NBA* in " + (end - start) + " milliseconds.");
+        path3.forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Algorithms agree: " +
+                (path1.equals(path2) && path1.equals(path3)));
+    }
+
+    private static DirectedGraph getRandomGraph(int nodes, 
+                                                int arcs, 
+                                                Random random) {
+        DirectedGraph graph = new DirectedGraph();
+
+        for (int id = 0; id < nodes; ++id) {
+            graph.addNode(id);
+        }
+
+        List<Integer> graphNodeList = new ArrayList<>(graph.getNodeList());
+
+        while (arcs-- > 0) {
+            Integer tailNodeId = choose(graphNodeList, random);
+            Integer headNodeId = choose(graphNodeList, random);
+            graph.addArc(tailNodeId, headNodeId);
+        }
+
+        return graph;
+    }
+
+    private static DirectedGraphNodeCoordinates 
+        getCoordinates(DirectedGraph graph, Random random) {
+        DirectedGraphNodeCoordinates coordinates =
+                new DirectedGraphNodeCoordinates();
+
+        for (Integer nodeId : graph.getNodeList()) {
+            coordinates.put(nodeId, randomPoint(1000.0, 1000.0, random));
+        }
+
+        return coordinates;
+    }
+
+    private static DirectedGraphWeightFunction 
+        getWeightFunction(DirectedGraph graph,
+                          DirectedGraphNodeCoordinates coordinates) {
+        DirectedGraphWeightFunction weightFunction = 
+                new DirectedGraphWeightFunction();
+
+        for (Integer nodeId : graph.getNodeList()) {
+            Point2D.Double p1 = coordinates.get(nodeId);
+
+            for (Integer childNodeId : graph.getChildrenOf(nodeId)) {
+                Point2D.Double p2 = coordinates.get(childNodeId);
+                double distance = p1.distance(p2);
+                weightFunction.put(nodeId, childNodeId, 1.1 * distance);
+            }
+        }
+
+        return weightFunction;
+    }
+
+    private static Point2D.Double randomPoint(double width,
+                                              double height,
+                                              Random random) {
+        return new Point2D.Double(width * random.nextDouble(),
+                                  height * random.nextDouble());
+    }
+
+    private static <T> T choose(List<T> list, Random random) {
+        return list.get(random.nextInt(list.size()));
+    }
+}
---
+++ src/main/java/net/coderodde/graph/pathfinding/support/NBAStarPathfinder.java
@@ -0,0 +1,229 @@
+package net.coderodde.graph.pathfinding.support;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.PriorityQueue;
+import java.util.Set;
+import net.coderodde.graph.DirectedGraph;
+import net.coderodde.graph.DirectedGraphWeightFunction;
+import net.coderodde.graph.pathfinding.AbstractPathfinder;
+import net.coderodde.graph.pathfinding.HeuristicFunction;
+
+/**
+ * This pathfinding algorithm is due to Wim Pijls and Henk Post in "Yet another
+ * bidirectional algorithm for shortest paths." 15 June 2009.
+ * <p>
+ * <b>This class is not thread-safe.</b> If you need it in different threads,
+ * make sure each thread has its own object of this class.
+ *
+ * @author Rodion "rodde" Efremov
+ * @version 1.6 (Oct 6, 2016)
+ */
+public final class NBAStarPathfinder extends AbstractPathfinder {
+
+    private final HeuristicFunction heuristicFunction;
+    private final PriorityQueue<HeapEntry> OPENA = new PriorityQueue<>();
+    private final PriorityQueue<HeapEntry> OPENB = new PriorityQueue<>();
+    private final Map<Integer, Integer> PARENTSA = new HashMap<>();
+    private final Map<Integer, Integer> PARENTSB = new HashMap<>();
+    private final Map<Integer, Double> DISTANCEA = new HashMap<>();
+    private final Map<Integer, Double> DISTANCEB = new HashMap<>();
+    private final Set<Integer> CLOSED = new HashSet<>();
+
+    private double fA;
+    private double fB;
+    private double bestPathLength;
+    private Integer touchNode;
+    private Integer sourceNodeId;
+    private Integer targetNodeId;
+
+    public NBAStarPathfinder(DirectedGraph graph,
+            DirectedGraphWeightFunction weightFunction,
+            HeuristicFunction heuristicFunction) {
+        super(graph, weightFunction);
+        this.heuristicFunction
+                = Objects.requireNonNull(heuristicFunction,
+                        "The input heuristic function is null.");
+    }
+
+    @Override
+    public List<Integer> search(int sourceNodeId, int targetNodeId) {
+        if (sourceNodeId == targetNodeId) {
+            return new ArrayList<>(Arrays.asList(sourceNodeId));
+        }
+
+        init(sourceNodeId, targetNodeId);
+
+        while (!OPENA.isEmpty() && !OPENB.isEmpty()) {
+            if (OPENA.size() < OPENB.size()) {
+                expandInForwardDirection();
+            } else {
+                expandInBackwardDirection();
+            }
+        }
+
+        if (touchNode == null) {
+            return new ArrayList<>();
+        }
+
+        return tracebackPath(touchNode, PARENTSA, PARENTSB);
+    }
+
+    private void expandInForwardDirection() {
+        Integer currentNode = OPENA.remove().getNode();
+
+        if (CLOSED.contains(currentNode)) {
+            return;
+        }
+
+        CLOSED.add(currentNode);
+
+        if (DISTANCEA.get(currentNode) +
+                heuristicFunction.estimateDistanceBetween(currentNode,
+                                                          targetNodeId)
+                >= bestPathLength
+                ||
+                DISTANCEA.get(currentNode) +
+                fB - 
+                heuristicFunction.estimateDistanceBetween(currentNode,
+                                                          sourceNodeId)
+                >= bestPathLength) {
+            // Reject the 'currentNode'.
+        } else {
+            // Stabilize the 'currentNode'.
+            for (Integer childNode : graph.getChildrenOf(currentNode)) {
+                if (CLOSED.contains(childNode)) {
+                    continue;
+                }
+
+                double tentativeDistance
+                        = DISTANCEA.get(currentNode)
+                        + weightFunction.get(currentNode, childNode);
+
+                if (!DISTANCEA.containsKey(childNode)
+                        || 
+                        DISTANCEA.get(childNode) > tentativeDistance) {
+                    DISTANCEA.put(childNode, tentativeDistance);
+                    PARENTSA.put(childNode, currentNode);
+                    HeapEntry e
+                            = new HeapEntry(
+                                    childNode,
+                                    tentativeDistance
+                                    + heuristicFunction
+                                    .estimateDistanceBetween(childNode,
+                                                             targetNodeId));
+                    OPENA.add(e);
+
+                    if (DISTANCEB.containsKey(childNode)) {
+                        double pathLength = tentativeDistance
+                                + DISTANCEB.get(childNode);
+
+                        if (bestPathLength > pathLength) {
+                            bestPathLength = pathLength;
+                            touchNode = childNode;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!OPENA.isEmpty()) {
+            fA = OPENA.peek().getDistance();
+        }
+    }
+
+    private void expandInBackwardDirection() {
+        Integer currentNode = OPENB.remove().getNode();
+
+        if (CLOSED.contains(currentNode)) {
+            return;
+        }
+
+        CLOSED.add(currentNode);
+
+        if (DISTANCEB.get(currentNode) +
+                heuristicFunction
+                .estimateDistanceBetween(currentNode,
+                                         sourceNodeId)
+                >= bestPathLength
+                || 
+                DISTANCEB.get(currentNode) +
+                fA -
+                heuristicFunction
+                .estimateDistanceBetween(currentNode, targetNodeId)
+                >= bestPathLength) {
+            // Reject the node 'currentNode'.
+        } else {
+            for (Integer parentNode : graph.getParentsOf(currentNode)) {
+                if (CLOSED.contains(parentNode)) {
+                    continue;
+                }
+
+                double tentativeDistance
+                        = DISTANCEB.get(currentNode)
+                        + weightFunction.get(parentNode, currentNode);
+
+                if (!DISTANCEB.containsKey(parentNode)
+                        || DISTANCEB.get(parentNode)
+                        > tentativeDistance) {
+                    DISTANCEB.put(parentNode, tentativeDistance);
+                    PARENTSB.put(parentNode, currentNode);
+                    HeapEntry e
+                            = new HeapEntry(parentNode,
+                                    tentativeDistance
+                                    + heuristicFunction
+                                    .estimateDistanceBetween(parentNode,
+                                                             targetNodeId));
+                    OPENB.add(e);
+
+                    if (DISTANCEA.containsKey(parentNode)) {
+                        double pathLength = tentativeDistance
+                                + DISTANCEA.get(parentNode);
+
+                        if (bestPathLength > pathLength) {
+                            bestPathLength = pathLength;
+                            touchNode = parentNode;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!OPENB.isEmpty()) {
+            fB = OPENB.peek().getDistance();
+        }
+    }
+
+    private void init(Integer sourceNodeId, Integer targetNodeId) {
+        OPENA.clear();
+        OPENB.clear();
+        PARENTSA.clear();
+        PARENTSB.clear();
+        DISTANCEA.clear();
+        DISTANCEB.clear();
+        CLOSED.clear();
+
+        double totalDistance
+                = heuristicFunction.estimateDistanceBetween(sourceNodeId,
+                        targetNodeId);
+
+        fA = totalDistance;
+        fB = totalDistance;
+        bestPathLength = Double.MAX_VALUE;
+        touchNode = null;
+        this.sourceNodeId = sourceNodeId;
+        this.targetNodeId = targetNodeId;
+
+        OPENA.add(new HeapEntry(sourceNodeId, fA));
+        OPENB.add(new HeapEntry(targetNodeId, fB));
+        PARENTSA.put(sourceNodeId, null);
+        PARENTSB.put(targetNodeId, null);
+        DISTANCEA.put(sourceNodeId, 0.0);
+        DISTANCEB.put(targetNodeId, 0.0);
+    }
+}
